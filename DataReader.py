bf972824dea34306e815b1fd091e42dc267f65c7
Ricardo <rrmonjars3@gatech.edu>
2020-05-03 15:54:44 -0600

    Added logo png, along with the initial framework of the GUI.

---
+++ DataReader.py
@@ -0,0 +1,16 @@
+# This class..
+
+import numpy as np
+import pandas as pd
+
+class Reader:
+    def __init__(self):
+        self.path
+        pass
+
+    def change_files_directory(self, new_path: str) -> None:
+        pass
+
+    def read_file(self, path: str) -> None:
+        pass
+
